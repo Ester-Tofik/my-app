78bd7b244e11a9ce4a85ce256d070f78e2c9be7d
אילה פרידמן <325191575@mby.co.il>
2022-03-27 18:35:28 +0300

    27.03.2022 view changes in logIn

--- src/components/logIn.js
+++ src/components/logIn.js
@@ -1,9 +1,4 @@
-import React, { useState } from 'react';
-import { InputText } from 'primereact/inputtext';
-import { InputNumber } from 'primereact/inputnumber';
-import { Button } from 'primereact/button';
-import { Checkbox } from 'primereact/checkbox';
-import { RadioButton } from 'primereact/radiobutton';
+import React from 'react';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
@@ -16,21 +11,18 @@ export default function LogIn() {
     return (
         <div className="wrapper fadeInDown">
             <div id="formContent">
-
                 <div className="fadeIn first">
                     <img src={logo} id="icon" alt="User Icon" />
                 </div>
-
                 <form>
-                    <input type="text" id="login" className="fadeIn second" name="login" placeholder="Identity" />
-                    <input type="text" id="password" className="fadeIn third" name="login" placeholder="Password" />
+                    <input type="text" id="login" className="fadeIn second" name="login" placeholder="Identity" /><br />
+                    <input type="password" id="password" className="fadeIn third" name="login" placeholder="Password" />
+                    <br /><br />
                     <input type="submit" className="fadeIn fourth" value="Log In" />
                 </form>
-
                 <div id="formFooter">
-                    <a className="underlineHover" href="#">Forgot Password?</a>
+                    <a className="underlineHover" href="#">create new user</a>
                 </div>
-
             </div>
         </div>
     );
