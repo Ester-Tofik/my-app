05537c7acaf019bc9b46db15c4fb14bcfc68c6da
אסתר טופיק <325202349@mby.co.il>
2022-05-10 17:50:36 +0300

    index marge

--- src/index.js
+++ src/index.js
@@ -11,15 +11,13 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import HomePage from "./components/homePage";
 import MenuButton from './components/menu';
 import EditUserDetails from './components/editUserDetails';
-<<<<<<< HEAD
 import Medicines from './components/medicince';
 import Indices from './components/indices';
 import Information from './components/information';
 import DrinkingWater from './components/DrinkingWater';
 import AskADoctor from './components/AskADoctor';
-=======
+
 import ReminderManagement from './components/ReminderManagement';
->>>>>>> 209b3f1377124b9ca54787c331d98c9d022219b3
 
 const store = createStore(userReducer, applyMiddleware());
 
@@ -32,17 +30,15 @@ ReactDOM.render(
       <Route path='/signUp' component={SignUp} />
       <Route path='/home' component={HomePage} />
       <Route path='/menu' component={MenuButton} />
-<<<<<<< HEAD
+
       <Route path='/edit' component={EditUserDetails} />
       <Route path='/medicince' component={Medicines} />
       <Route path='/indices' component={Indices} />
       <Route path='/information' component={Information} />
       <Route path='/drinkingWater' component={DrinkingWater} />
       <Route path='/askADoctor' component={AskADoctor} />
-=======
       <Route path='/edit' component={EditUserDetails}/>
       <Route path='/reminderManagement' component={ReminderManagement}/>
->>>>>>> 209b3f1377124b9ca54787c331d98c9d022219b3
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
