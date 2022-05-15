b00e28498ea47584dcc227f8cd7984d2c679659d
אסתר טופיק <325202349@mby.co.il>
2022-05-15 21:23:16 +0300

    aaaa

--- src/components/medicationDetails.js
+++ src/components/medicationDetails.js
@@ -3,12 +3,14 @@ import Search from './search'
 
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
+import { Button } from '@material-ui/core';
 
 
 
 export default function MedicationDetails() {
     const [age, setAge] = React.useState('');
     const [amount, setAmount] = React.useState(' ');
+    const [show, setShow] = React.useState(false);
 
     const handleChange = (event) => {
         setAge(event.target.value);
@@ -35,6 +37,10 @@ export default function MedicationDetails() {
                         <TextField id="outlined-number" label="Number" type="number" onChange={e=>setAmount(Number(e.target.value))}
                          InputLabelProps={{ inputProps:{min:'0',max:'10',step:'1'} }} />
                          : מס' פעמים ביום
+                         <Button variant='contained' startIcon={<AddIcon/>} onClick={()=>setShow(true)}>
+                        Add
+                         </Button>
+                         {show && <Houres amount={amount}/>}
                     </div>
                 </div>
             </div>
